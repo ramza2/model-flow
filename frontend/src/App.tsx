c5594bd68677fd189b5baa0e302b605ad7bd494f
ramza2 <chjeon@openlink.kr>
2026-09-17 11:23:31 +0900

    feat: compose preparation lifecycle handoff page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import ProjectCreate from "./pages/ProjectCreate";
 import ProjectOverview from "./pages/ProjectOverview";
 import DataSources from "./pages/DataSources";
 import Datasets from "./pages/Datasets";
-import PreparationBuilder from "./pages/PreparationBuilder";
+import PreparationLifecyclePage from "./pages/PreparationLifecyclePage";
 import Preparations from "./pages/Preparations";
 import DatasetDetail from "./pages/DatasetDetail";
 import JobCreate from "./pages/JobCreate";
@@ -48,7 +48,7 @@ function ProductRoutes() {
     <Route path="/projects/:projectId/datasets" element={<Datasets />} />
     <Route path="/projects/:projectId/datasets/:datasetId" element={<DatasetDetail />} />
     <Route path="/projects/:projectId/preparations" element={<Preparations />} />
-    <Route path="/projects/:projectId/preparations/:preparationId" element={<PreparationBuilder />} />
+    <Route path="/projects/:projectId/preparations/:preparationId" element={<PreparationLifecyclePage />} />
     <Route path="/projects/:projectId/experiments" element={<Runs />} />
     <Route path="/projects/:projectId/experiments/runs/:runId" element={<RunDetail />} />
     <Route path="/projects/:projectId/experiments/compare" element={<RunCompare />} />
